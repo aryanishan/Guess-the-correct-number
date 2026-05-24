5a16a6af4710e09692795265b1d5380731e7af6b
aryanishan <myteam034221@gmail.com>
2026-05-24 23:35:29 +0530

    added some new features

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -21,6 +21,9 @@ document.querySelector('.check').addEventListener('click', function () {
   }
   else if(guess === secretNumber){
     document.querySelector('.message').textContent = '🎉Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   }
   else if(guess > secretNumber){
     if(score > 1){
