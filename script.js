e3e9838ca16fbf1ab9c599019b97e184fc8bde43
aryanishan <myteam034221@gmail.com>
2026-05-24 23:50:12 +0530

    high score chaning logic implemented

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -24,6 +24,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if(score > highscore){
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   }
   else if(guess > secretNumber){
     if(score > 1){
