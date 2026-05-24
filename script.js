870e11cbab892412cdbf053692f08892ea85a919
aryanishan <myteam034221@gmail.com>
2026-05-24 22:57:38 +0530

    logic implemented

--- script.js
+++ script.js
@@ -23,13 +23,26 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🎉Correct Number!';
   }
   else if(guess > secretNumber){
-    document.querySelector('.message').textContent = '📈Too high!';
+    if(score > 1){
+      document.querySelector('.message').textContent = '📈Too high!';
     score--;
     document.querySelector('.score').textContent = score;
+    }
+    else{
+      document.querySelector('.message').textContent = '💥You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
   }
   else if(guess < secretNumber){
-    document.querySelector('.message').textContent = '📉Too low!';
+    if(score > 1){
+      document.querySelector('.message').textContent = '📉Too low!';
     score--;
     document.querySelector('.score').textContent = score;
+    }
+    else{
+      document.querySelector('.message').textContent = '💥You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
